9da72297194a338776c2b9fdeb2c54bfa4ab675a
Lavanya <laxmilavanya364@gmail.com>
2024-03-15 18:31:16 +0530

    Huh

---
+++ script.js
@@ -0,0 +1,25 @@
+const navLinks = document.querySelector(".nav-links");
+
+function Toggle(e) {
+    // console.log(e.name)
+    e.name = e.name === "menu" ? "close" : "menu"
+    navLinks.classList.toggle('top-[108px]')
+}
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay:3000
+    },
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+ 
